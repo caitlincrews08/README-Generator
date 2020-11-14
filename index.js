b36c8869f41e5fc0667cf4c7c183584c4fb8694e
Caitlin Crews <caitlinrcrews@gmail.com>
2020-11-13 21:19:53 -0500

    changed questions to an array and added write readme function

--- index.js
+++ index.js
@@ -1,61 +1,65 @@
 // packages necessary for generator to function
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
 
-// function that displays questions to user
-const promptUser = () =>
-inquirer.prompt([
-    {
-      type: 'input',
-      name: 'username',
-      message: 'What is your GitHub username?'
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?'
-    },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Enter a description for your project.'
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'What kind of license should your project have?',
-      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What command should be run to install dependencies?',
-      default: 'npm i'
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'What command should be run to run tests?',
-      default: 'npm test'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What does the user need to know about using the repo?'
-    },
-    {
-      type: 'input',
-      name: 'contribute',
-      message: 'What does the user need to know about contributing to the repo?'
-    }
-    // answers returned by user
-]).then(answers => {
-  console.log(answers)
-});
+// questions that will be displayed to user
+const questions = [
+  {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project.'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What command should be run to run tests?',
+    default: 'npm test'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?'
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'What does the user need to know about contributing to the repo?'
+  }
+];
 
-promptUser();
+// function that will write read me and throw error or success message
+function writeReadMe(questions, answers) {
+  fs.writeFile(questions, answers.join(""), function(err){
+    if(err) {
+      console.log(err)
+    } console.log("Generating README!");
+  });
+};
